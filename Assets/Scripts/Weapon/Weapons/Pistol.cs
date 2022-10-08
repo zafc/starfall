d0bc9dfbee6ee0327354ca540bf39a4b9c0bc2b4
zafc <jzhang7256@gmail.com>
2022-10-08 13:48:33 -0400

    started work on pistol

---
+++ Assets/Scripts/Weapon/Weapons/Pistol.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : RangedWeapon
+{
+    protected override void Fire(Vector3 dir){
+        
+    }
+}
